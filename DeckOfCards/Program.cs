106d206e1a66921c2b068642ae215cb24c984bca
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-07-07 22:29:23 +0530

    [Girish]Shuffle and distributing the cards

--- DeckOfCards/Program.cs
+++ DeckOfCards/Program.cs
@@ -4,9 +4,24 @@ namespace DeckOfCards
 {
     class Program
     {
+        public class Cards
+        {
+            public string suit;
+            public string rank;
+            public Cards(string suit, string rank)
+            {
+                this.suit = suit;
+                this.rank = rank;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Deck of cards Problem !!!");
+            CardDeck cardDeck = new CardDeck();
+            Cards[,] players = new Cards[4, 9];
+            players = cardDeck.alotCards(players);
+            cardDeck.display(players);
         }
+
     }
 }
